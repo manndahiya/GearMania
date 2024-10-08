8ff5fc952882e428b5b9742ac5ca330b97b06a9e
manndahiya <mannlakra10@gmail.com>
2024-10-08 09:44:35 +0530

    New Changes

---
+++ Assets/Script/Grid.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    public int width;
+    public int height;
+
+   public Grid(int width, int height)
+    {
+        this.width = width;
+        this.height = height;   
+
+    }
+}
